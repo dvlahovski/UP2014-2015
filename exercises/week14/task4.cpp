0c35710333c3c49ac11f531305dd2f349b1c4a73
eevlogieva <eevlogieva@yahoo.com>
2015-02-12 22:31:51 +0200

    Fixed task 4 and added example arrays

--- exercises/week14/task4.cpp
+++ exercises/week14/task4.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 //non-recursive solution
 
 bool isFirstHalfGreater(int a[], int n)
@@ -14,10 +17,6 @@ bool isFirstHalfGreater(int a[], int n)
 
 //recursive solution
 
-bool isFirstHalfGreaterR(int a[], int n)
-{
-    return helper(a, n, 0, n);
-}
 
 /*a helper function, the first two parameters of which are the array and the number n,
 the second two are iterators with the same function as i and j in the non-recursive solution*/
@@ -29,7 +28,7 @@ bool helper(int a[], int n, int i, int j)
         if (j==2*n)
         {
             //the i-th element of the array is greater than every element in the second half, so we call the function with i+1
-            return helper(a, n, i++, n);
+            return helper(a, n, ++i, n);
         }
         else //in this case n <= j <= 2*n-1
         {
@@ -37,9 +36,21 @@ bool helper(int a[], int n, int i, int j)
             else
             {
                 //a[i] > a[j], so we call the function with j+1(iterating through the second half of the array)
-                return helper(a, n, i, j++);
+                return helper(a, n, i, ++j);
             }
         }
     }
 
+}
+bool isFirstHalfGreaterR(int a[], int n)
+{
+    return helper(a, n, 0, n);
+}
+
+int main()
+{
+    int arr1[8] = {14, 21, 44, 32, 3, 9, 11, 0};
+    int arr2[8] = {14, 3, 5, 28, 44, 2, 1, 100};
+    cout << boolalpha << isFirstHalfGreaterR(arr1, 4) << " " << isFirstHalfGreaterR(arr2, 4);
+    return 0;
 }
